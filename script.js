00599532e1b5756b859f9408f41a7a2af832e459
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-12-21 15:55:29 +0530

    initialize project with Tailwind CSS setup and basic timer functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const workDuration = document.querySelector("#workDuration");
+const breakDuration = document.querySelector("#breakDuration");
+const isMinutes = document.querySelector("#minutes").value;
+const isHours = document.querySelector("#hours");
+const backBtn = document.querySelector("#backBtn");
+const nextBtn = document.querySelector("#nextBtn");
