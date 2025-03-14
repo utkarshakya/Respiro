d90e075cef63c5cb9b67a863a6aa5bf378452ac5
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-15 05:28:22 +0530

    feat: restructure Home component layout, add Footer component for improved presentation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { CardSection, CustomTimer, Heading, Navbar } from "./partial";
+import Footer from "./partial/Footer";
 
 function Home() {
   return (
     <>
-      <div className="w-full h-full flex flex-col gap-5">
-        <Navbar />
-        <Heading />
-        <CardSection />
-        <CustomTimer />
+      <Navbar />
+      <div className="w-screen max-w-7xl flex flex-col items-center gap-5 bg-slate-800 mx-auto px-5 md:px-20">
+        <div className="w-full h-full flex flex-col gap-5">
+          <Heading />
+          <CardSection />
+          <CustomTimer />
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/partial/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full flex justify-center bg-gray-900 py-10">
+      <p className="text-sm">Made With ❤️ By Utkarsh Shakya</p>
+    </div>
+  );
+}
+
+export default Footer;
