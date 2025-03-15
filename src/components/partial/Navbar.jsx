56a0809570f570bdefb571e32f9a1ae7ea40ae06
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-15 05:32:27 +0530

    feat: update Navbar styling for improved visual consistency and responsiveness

--- src/components/partial/Navbar.jsx
+++ src/components/partial/Navbar.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <div className="w-full py-5 flex justify-between flex-wrap gap-3 text-xs border-b border-gray-500">
+      <div className="w-full max-w-7xl p-5 flex justify-between flex-wrap gap-3 text-xs mx-auto">
         <div className="flex gap-3 flex-wrap font-semibold">
-          <button className="py-3 px-5 bg-gray-900/50 text-gray-300 cursor-pointer hover:bg-gray-900 hover:text-white rounded-md">
+          <button className="py-3 px-5 bg-gray-800/50 text-gray-300 cursor-pointer hover:bg-gray-800 hover:text-white rounded-md">
             Home
           </button>
-          <button className="py-3 px-5 bg-gray-900/50 text-gray-300 cursor-pointer hover:bg-gray-900 hover:text-white rounded-md">
+          <button className="py-3 px-5 bg-gray-800/50 text-gray-300 cursor-pointer hover:bg-gray-800 hover:text-white rounded-md">
             About
           </button>
         </div>
         <div>
-          <button className="hidden md:block py-3 px-5 bg-gray-900/50 text-gray-300 cursor-pointer hover:bg-gray-900 hover:text-white rounded-md italic">
+          <button className="hidden md:block py-3 px-5 bg-gray-800/50 text-gray-300 cursor-pointer hover:bg-gray-800 hover:text-white rounded-md italic">
             Why Breaks?
           </button>
-          <button className="md:hidden py-3 px-5 bg-gray-900/50 text-gray-300 cursor-pointer hover:bg-gray-900 hover:text-white rounded-md italic">
+          <button className="md:hidden py-3 px-5 bg-gray-800/50 text-gray-300 cursor-pointer hover:bg-gray-800 hover:text-white rounded-md italic">
             ?
           </button>
         </div>
